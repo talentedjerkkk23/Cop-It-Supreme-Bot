707d7dbd2862b6043adf5b19cb7f96afcea62233
carl1to <c@live.fr>
2017-04-11 12:23:53 +0200

    v1.0.10

---
+++ src/js/supreme/checkout.js
@@ -0,0 +1,30 @@
+const fillCheckout = () => {
+	chrome.runtime.sendMessage({msg: "privateData"}, function(data) {
+		var r = JSON.parse(data)
+		
+		chrome.runtime.sendMessage({msg: "params"}, function(settings) {
+
+			//this remove captcha but not stable, payment failed sometimes
+			if (settings.removeCaptcha)
+				$(".g-recaptcha").remove()
+
+			$('#order_billing_name').val(r.name)
+			$('#order_billing_country').val(r.country)
+			$('#order_tel').val(r.phone)
+			$('#order_email').val(r.email)
+			$('#bo').val(r.address1)
+			$('#oba3').val(r.address2)
+			$('#order_billing_address_3').val(r.address3)
+			$('#bo').val(r.address)
+			$('#order_billing_city').val(r.city)
+			$('#order_billing_zip').val(r.zip)
+			$('#credit_card_type').val(r.card_type)
+			$('#cnb').val(r.card_number)
+			$('#vval').val(r.cvv)
+			$('#credit_card_month').val(r.card_month)
+			$('#credit_card_year').val(r.card_year)
+			$(".icheckbox_minimal").click()
+			$('[name=commit]').click()
+		})
+	})
+}
